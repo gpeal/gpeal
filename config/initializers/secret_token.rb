487ee9867e549267146307b29893f434f76c99e5
Gabriel Peal <gpeal@u.northwestern.edu>
2012-03-09 22:04:56 -0800

    Initialized rails app. Switched to postgre

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Gpeal::Application.config.secret_token = '548bd302602b2eb9dafe0ba9ed1124b51c8da298310dc11ed1aae26aed64a445d414a0081e51d0c543f3caf19eb8df3f13afd57b1492a6f98d14a9a2f61098b9'
